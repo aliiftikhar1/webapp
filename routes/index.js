22c759dce69b022c9f26f4c4122c3374180b6e38
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-07-22 07:09:57 +0500

    new

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-// routes/index.js
-const express = require('express');
-const router = express.Router();
+const trimRequest = require("trim-request");
 
-router.get('/', (req, res) => {
-  res.send('Hello, Vercel!');
-});
+class Routes {
+  constructor(app) {
+    this.app = app;
+  }
 
-module.exports = router;
+  /* creating app Routes starts */
+  appRoutes() {
+    this.app.use("/auth", trimRequest.all, require("./auth"));
+    this.app.use("/dashboard", trimRequest.all, require("./dashboard"));
+    this.app.use("/store", trimRequest.all, require("./store"));
+    this.app.use("/category", trimRequest.all, require("./categories"));
+    this.app.use("/type", trimRequest.all, require("./type"));
+      this.app.use("/medicine", trimRequest.all, require("./medicine"));
+  }
+
+  routesConfig() {
+    this.appRoutes();
+  }
+}
+
+module.exports = Routes;
